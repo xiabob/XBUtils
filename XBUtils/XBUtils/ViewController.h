600d35e66455c71e2721ec9ca4d5eed6425cc66c
xiabob <xiabob@yeah.net>
2016-08-23 11:49:39 +0800

    first init and add XBWeakProxy

---
+++ XBUtils/XBUtils/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XBUtils
+//
+//  Created by xiabob on 16/8/23.
+//  Copyright © 2016年 xiabob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XBUtils/XBUtils/XBWeakProxy.h
@@ -0,0 +1,18 @@
+//
+//  XBWeakProxy.h
+//  XBUtils
+//
+//  Created by xiabob on 16/8/23.
+//  Copyright © 2016年 xiabob. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XBWeakProxy : NSObject
+
+@property (nonatomic, weak, readonly) id target;
+
+- (instancetype)initWithTarget:(id)target;
++ (instancetype)proxyWithTarget:(id)target;
+
+@end
