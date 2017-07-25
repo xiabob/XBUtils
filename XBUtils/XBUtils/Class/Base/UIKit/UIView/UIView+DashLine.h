321201ff07201e58dc1ea39e61763535d6252232
xiabob <xiabob@yeah.net>
2017-07-25 15:57:27 +0800

    添加绘制虚线（包括水平、垂直两个方向）的分类

---
+++ XBUtils/XBUtils/Class/Base/UIKit/UIView/UIView+DashLine.h
@@ -0,0 +1,22 @@
+//
+//  UIView+DashLine.h
+//  XBUtils
+//
+//  Created by xiabob on 2017/7/25.
+//  Copyright © 2017年 xiabob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (DashLine)
+
+/**
+ 绘制虚线，如果view的width大于height，绘制水平虚线；如果view的height大于width，绘制垂直虚线。
+
+ @param dashLength 虚线的长度
+ @param dashSpacing 虚线间的间距
+ @param lineColor 绘制虚线使用的颜色
+ */
+- (void)drawDashLineWithDashLength:(CGFloat)dashLength dashSpacing:(CGFloat)dashSpacing dashColor:(UIColor *)lineColor;
+
+@end
---
+++ XBUtils/XBUtils/Class/Base/UIKit/UIView/UIView+DashLine.m
@@ -0,0 +1,46 @@
+//
+//  UIView+DashLine.m
+//  XBUtils
+//
+//  Created by xiabob on 2017/7/25.
+//  Copyright © 2017年 xiabob. All rights reserved.
+//
+
+#import "UIView+DashLine.h"
+
+@implementation UIView (DashLine)
+
+- (void)drawDashLineWithDashLength:(CGFloat)dashLength dashSpacing:(CGFloat)dashSpacing dashColor:(UIColor *)lineColor {
+    if ([[self layer] sublayers].count > 0) {
+        for (CALayer *layer in [[self layer] sublayers]) {
+            [layer removeFromSuperlayer];
+        }
+    }
+    
+    CGFloat lineWidth = MIN(CGRectGetWidth(self.frame), CGRectGetHeight(self.frame));
+    
+    CAShapeLayer *shapeLayer = [CAShapeLayer layer];
+    [shapeLayer setBounds:self.bounds];
+    [shapeLayer setPosition:CGPointMake(CGRectGetWidth(self.frame) / 2, CGRectGetHeight(self.frame) / 2)];
+    [shapeLayer setFillColor:[UIColor clearColor].CGColor];
+    [shapeLayer setStrokeColor:lineColor.CGColor];
+    [shapeLayer setLineWidth:lineWidth];
+    [shapeLayer setLineCap:kCALineCapRound];
+    [shapeLayer setLineDashPattern:[NSArray arrayWithObjects:[NSNumber numberWithInt:dashLength], [NSNumber numberWithInt:dashSpacing], nil]];
+    
+    CGMutablePathRef path = CGPathCreateMutable();
+    CGPathMoveToPoint(path, NULL, 0, 0);
+    if (CGRectGetWidth(self.frame) > CGRectGetHeight(self.frame)) {
+        //水平虚线
+        CGPathAddLineToPoint(path, NULL, CGRectGetWidth(self.frame), 0);
+    } else {
+        //垂直虚线
+        CGPathAddLineToPoint(path, NULL, 0, CGRectGetHeight(self.frame));
+    }
+    [shapeLayer setPath:path];
+    CGPathRelease(path);
+    
+    [self.layer addSublayer:shapeLayer];
+}
+
+@end
